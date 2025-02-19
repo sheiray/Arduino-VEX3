ae5480031a3b51d1213c90b385ce47cb7726135e
Mark Mercer <Mark.Mercer@epsb.ca>
2025-02-19 13:26:08 -0700

    Update Blink.ino

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -6,7 +6,7 @@ void loop() {
 on(1000, 500); // ON 1s (1000ms), OFF 0.5s (500 ms)
 }
 
-void on(int x, y){
+void on(int x, y){ //ERROR on Parameter for instruction
   digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
   delay(x);
   off(y);
